aace4a0ae0d32385d3e0f1178230b6962125902a
Javohir544 <javohirtursunov077@gmail.com>
2026-08-22 23:01:39 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
+
     socket.on("join-room", (room) => {
         socket.join(room);
     });
@@ -19,8 +29,11 @@ io.on("connection", (socket) => {
             text: data.text
         });
     });
+
 });
 
-server.listen(3000, () => {
-    console.log("Сайт запущен: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
